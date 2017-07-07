6f4c8683e42adf66d49ed594d2b00049c057fb72
shadre <shadi.rezek@gmail.com>
2017-07-07 13:57:47 +0200

    Add a Hand class with basic interface
    * Modify Card class for handling possible value reductions

--- game.rb
+++ game.rb
@@ -28,13 +28,19 @@ class Deck
 end
 
 class Card
-  SYMBOLS = { c: "♣", d: "♦", h: "♥", s: "♠" }
+  FACE_VALUES = { j: 10, q: 10, k: 10, a: 11 }
+  REDUCTIONS  = { a: 10 }
+  SYMBOLS     = { c: "♣", d: "♦", h: "♥", s: "♠" }
 
-  attr_reader :rank, :suit
+  attr_reader :rank, :suit, :reduction
 
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
+  end
+
+  def reduction
+    REDUCTIONS[rank]
   end
 
   def suit_symbol
@@ -44,7 +50,50 @@ class Card
   def to_s
     rank.to_s.upcase + suit_symbol
   end
+
+  def value
+    FACE_VALUES[rank] || rank
+  end
 end
 
 class Hand
+  VALUE_THRESHOLD = 21
+
+  def initialize
+    @cards = []
+  end
+
+  def <<(card)
+    cards << card
+  end
+
+  def busted?
+    total > VALUE_THRESHOLD
+  end
+
+  def to_s
+    cards.map(&:to_s).join(" ")
+  end
+
+  def total
+    curr_total = unreduced_total
+
+    possible_reductions.sort.each do |reduction|
+      curr_total -= reduction if curr_total > VALUE_THRESHOLD
+    end
+
+    curr_total
+  end
+
+  private
+
+  attr_reader :cards
+
+  def possible_reductions
+    cards.map(&:reduction).select(&:itself)
+  end
+
+  def unreduced_total
+    cards.map(&:value).reduce(:+)
+  end
 end
